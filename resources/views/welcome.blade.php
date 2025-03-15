83e87b17a1499f47cfd72fd15bc4b51192a13bac
Anton Yuriv <yurivanton@gmail.com>
2025-03-15 17:43:53 +0100

    qw1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
    
 
         <!-- Styles / Scripts -->
-            @vite('resources/css/app.css')
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
