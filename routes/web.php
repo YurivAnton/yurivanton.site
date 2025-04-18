48daf2a5e441950c81c664584a1f32bdbd569662
yuriv anton <yurivanton@gmail.com>
2025-04-18 20:40:01 +0200

    t1

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Route::get('change', [LanguageController::class, 'change'])->name('lang.change');
 });
 
 require __DIR__.'/auth.php';
