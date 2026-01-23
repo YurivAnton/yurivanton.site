0fbf17b3bc0fb3df9685e4659d6aaf60c396fc6e
YurivAnton <64098332+YurivAnton@users.noreply.github.com>
2026-01-23 22:05:37 +0100

    Feature/save training stats (#5)
    * create multiplication training statistics table
    
    * Add TrainingStat model and user relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function TrainingStats()
+    {
+        return $this->hasMany(TrainingStat::class);
+    }
 }
